d321bd8c3be2673570aac890a93520fc9fd04a7e
hannacthe8 <hannac@the8camerons.ca>
2025-07-08 15:50:35 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          900: "#4b2e2e",
+          800: "#6b3e2e",
+          700: "#8b5e3c",
+        },
+        amber: require("tailwindcss/colors").amber,
+      },
+    },
   },
   plugins: [],
-};
+}
